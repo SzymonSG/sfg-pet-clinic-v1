687a6a61c520d7a7d46b0e7c094fbe0f60056194
SzymonSG <sg.szymon8@gmail.com>
2020-08-17 17:42:31 +0200

    Initial Project Add

--- src/test/java/scoty/springframework/sfgpetclinic/SfgPetClinicV2ApplicationTests.java
+++ src/test/java/scoty/springframework/sfgpetclinic/SfgPetClinicV2ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SfgPetClinicV2ApplicationTests {
+class   SfgPetClinicV2ApplicationTests {
 
     @Test
     void contextLoads() {
